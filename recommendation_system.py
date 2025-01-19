9ecf055c3a6bec63c0b783582c0a1903698e0895
MrBinit <sapkotabinit2002@gmail.com>
2025-01-19 15:51:17 +0545

    minor changes

--- recommendation_system.py
+++ recommendation_system.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+from collections import defaultdict
 
 ratings = pd.read_csv('/home/binit/Graph_neural_networks/ml-100k/u.data', sep = '\t',
                       names=['user_id', 'movie_id', 'rating', 'unix_timestamp'])
@@ -11,3 +12,11 @@ print(movies.head(10))
 
 ratings = ratings[ratings.rating >= 4]
 print(ratings)
+
+pairs = defaultdict(int)
+for group in ratings.groupby("userID"):
+    user_movies = list(group[1]["movieId"])
+    for i in range(len(user_movies)):
+        for j in range(i+1, len(user_movies)):
+            pairs[(user_movies[i], user_movies[j])]+=1
+            
