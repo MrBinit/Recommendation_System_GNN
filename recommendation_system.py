14d58e894990b632204e162ae97a9be4c80ee299
MrBinit <sapkotabinit2002@gmail.com>
2025-01-19 16:01:51 +0545

    solved the issue with userid as it result both key and value and I just need values as a result I was facing the issue

--- recommendation_system.py
+++ recommendation_system.py
@@ -1,22 +1,38 @@
-import pandas as pd 
+import pandas as pd
 from collections import defaultdict
+import networkx as nx
 
-ratings = pd.read_csv('/home/binit/Graph_neural_networks/ml-100k/u.data', sep = '\t',
-                      names=['user_id', 'movie_id', 'rating', 'unix_timestamp'])
+ratings = pd.read_csv(
+    '/home/binit/Graph_neural_networks/ml-100k/u.data',
+    sep='\t',
+    names=['user_id', 'movie_id', 'rating', 'unix_timestamp']
+)
 print(ratings.head(10))
 
-movies = pd.read_csv('/home/binit/Graph_neural_networks/ml-100k/u.item', sep = '|',
-                     usecols=range(2), names=['movie_id', 'title'],encoding='latin-1')
-
+movies = pd.read_csv(
+    '/home/binit/Graph_neural_networks/ml-100k/u.item',
+    sep='|',
+    usecols=range(2),
+    names=['movie_id', 'title'],
+    encoding='latin-1'
+)
 print(movies.head(10))
 
 ratings = ratings[ratings.rating >= 4]
 print(ratings)
 
 pairs = defaultdict(int)
-for group in ratings.groupby("userID"):
-    user_movies = list(group[1]["movieId"])
+
+#Increment counter specific to a pair of movies every time they are seen together in the same list
+for _, group in ratings.groupby("user_id"):
+    user_movies = list(group["movie_id"])
     for i in range(len(user_movies)):
-        for j in range(i+1, len(user_movies)):
-            pairs[(user_movies[i], user_movies[j])]+=1
-            
+        for j in range(i + 1, len(user_movies)):
+            pairs[(user_movies[i], user_movies[j])] += 1
+#till now it would store number of times two movies has been liked by the same user 
+
+G = nx.Graph()
+
+for pair in pairs:
+    
+
